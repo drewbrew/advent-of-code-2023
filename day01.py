38f096b521a107090ebaa8344e5c3910367e4f0b
Drew Winstel <drew@hsv.beer>
2023-12-01 10:40:56 -0600

    Add an alternate solution using regexes that's much faster
    8.79 ms vs 679 microsec according to ipython

--- day01.py
+++ day01.py
@@ -1,3 +1,4 @@
+import re
 from pathlib import Path
 
 TEST_INPUT = """1abc2
@@ -25,9 +26,43 @@ def part1(puzzle_input: str) -> int:
     return total
 
 
+def part2_alt(puzzle_input: str) -> int:
+    values = {
+        "one": 1,
+        "two": 2,
+        "three": 3,
+        "four": 4,
+        "five": 5,
+        "six": 6,
+        "seven": 7,
+        "eight": 8,
+        "nine": 9,
+        "zero": 0,
+    } | {str(i): i for i in range(10)}
+    regex = re.compile(rf'({"|".join(values)})')
+    reversed_regex = re.compile(rf'({"|".join(i[::-1] for i in values)})')
+    total = 0
+    for line in puzzle_input.splitlines():
+        first_digit = values[regex.search(line).group(0)]
+        last_digit = values[reversed_regex.search(line[::-1]).group(0)[::-1]]
+        score = first_digit * 10 + last_digit
+        total += score
+    return total
+
+
 def part2(puzzle_input: str) -> int:
     lines = [line.strip() for line in puzzle_input.splitlines()]
     total = 0
+    for line in lines:
+        digits = digits_in_line(line)
+
+        value = digits[0] * 10 + digits[-1]
+        # print(line, value)
+        total += value
+    return total
+
+
+def digits_in_line(line: str) -> list[int]:
     values = {
         "one": 1,
         "two": 2,
@@ -41,27 +76,22 @@ def part2(puzzle_input: str) -> int:
         "zero": 0,
     } | {str(i): i for i in range(10)}
     first_chars = {char[0] for char in values}
-    for line in lines:
-        digits = []
-        for index, char in enumerate(line):
-            if char in first_chars:
-                # print(char, line[index : index + 5])
-                if char.isdigit():
-                    digits.append(int(char))
-                    continue
-                for offset in range(3, 6):
-                    try:
-                        value = values[line[index : index + offset]]
-                    except KeyError:
-                        pass
-                    else:
-                        digits.append(value)
-                        continue
-
-        value = digits[0] * 10 + digits[-1]
-        # print(line, value)
-        total += value
-    return total
+    digits = []
+    for index, char in enumerate(line):
+        if char in first_chars:
+            # print(char, line[index : index + 5])
+            if char.isdigit():
+                digits.append(int(char))
+                continue
+            for offset in range(3, 6):
+                try:
+                    value = values[line[index : index + offset]]
+                except KeyError:
+                    pass
+                else:
+                    digits.append(value)
+                    break
+    return digits
 
 
 def main():
@@ -72,6 +102,8 @@ def main():
     print(part1(real_input))
     part_2_result = part2(PART_2_TEST_INPUT)
     assert part_2_result == 281, part_2_result
+    part_2_alt_version = part2_alt(PART_2_TEST_INPUT)
+    assert part_2_alt_version == 281, part_2_alt_version
     print(part2(real_input))
 
 
