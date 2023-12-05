560e04e52cad30f2092efe5bd5cf62ff627d655a
Drew Winstel <drew@hsv.beer>
2023-12-05 11:11:24 -0600

    Day 5: a real part 2 solution
    And also fixes an off-by-one error

--- day05.py
+++ day05.py
@@ -1,7 +1,9 @@
+"""Day 5: needlessly complicated almanacs"""
 from dataclasses import dataclass
 from itertools import batched
 from pathlib import Path
 
+from intervaltree import Interval, IntervalTree
 
 REAL_INPUT = Path("day05.txt").read_text()
 
@@ -44,12 +46,17 @@ class Mapping:
     def __init__(self, ranges: list[tuple[int, int, int]] | None = None):
         self.ranges = ranges or []
 
-    def add_range(self, source_start: int, destination_start: int, range_: int):
+    def add_range(
+        self,
+        source_start: int,
+        destination_start: int,
+        range_: int,
+    ):
         self.ranges.append((source_start, destination_start, range_))
 
     def __getitem__(self, obj: int) -> int:
         for source_start, dest_start, range_ in self.ranges:
-            if obj in range(source_start, source_start + range_ + 1):
+            if obj in range(source_start, source_start + range_):
                 offset = obj - source_start
                 return dest_start + offset
         return obj
@@ -69,19 +76,6 @@ class Garden:
     temperature_to_humidity: Mapping
     humidity_to_location: Mapping
 
-    def next_seed(self, part2: bool = False):
-        if not part2:
-            iterator = iter(self.seeds)
-            while True:
-                try:
-                    yield next(iterator)
-                except StopIteration:
-                    return
-        else:
-            for start, length in batched(self.seeds, n=2):
-                for seed in range(start, start + length + 1):
-                    yield seed
-
 
 def parse_input(puzzle_input: str) -> Garden:
     seed_line, *groups = puzzle_input.split("\n\n")
@@ -93,14 +87,18 @@ def parse_input(puzzle_input: str) -> Garden:
         map_name = header.split()[0].replace("-", "_")
         kwargs[map_name] = Mapping()
         for line in lines:
-            destination_start, source_start, range_ = [int(i) for i in line.split()]
+            (
+                destination_start,
+                source_start,
+                range_,
+            ) = [int(i) for i in line.split()]
             kwargs[map_name].add_range(source_start, destination_start, range_)
-    print(kwargs)
+    # print(kwargs)
     garden = Garden(**kwargs)
     return garden
 
 
-def part1(puzzle_input: str, part2: bool = False):
+def part1(puzzle_input: str):
     garden = parse_input(puzzle_input)
     if puzzle_input == TEST_INPUT:
         expected = {79: 81, 14: 14, 55: 57, 13: 13}
@@ -111,11 +109,8 @@ def part1(puzzle_input: str, part2: bool = False):
                 garden.seed_to_soil[seed],
             )
     min_location = 10_000_000_000
-    if part2:
-        total = sum(garden.seeds[i] for i in range(1, len(garden.seeds), 2))
-        print("processing " f"{total}" " seeds")
-        seeds_processed = 0
-    for seed in garden.next_seed(part2=part2):
+
+    for seed in garden.seeds:
         soil = garden.seed_to_soil[seed]
         fertilizer = garden.soil_to_fertilizer[soil]
         water = garden.fertilizer_to_water[fertilizer]
@@ -123,26 +118,58 @@ def part1(puzzle_input: str, part2: bool = False):
         temperature = garden.light_to_temperature[light]
         humidity = garden.temperature_to_humidity[temperature]
         location = garden.humidity_to_location[humidity]
-        if part2:
-            seeds_processed += 1
-            if not seeds_processed % 100_000:
-                print(
-                    f"processed {seeds_processed} "
-                    f"({seeds_processed * 100 / total:.4f}%), {min_location=}",
-                    end="\r",
-                )
         min_location = min(min_location, location)
-    print()
+
     return min_location
+
+
+def part2(puzzle_input: str) -> int:
+    garden = parse_input(puzzle_input)
+    seed_intervals = IntervalTree(
+        [
+            Interval(start, end + start)
+            for start, end in batched(
+                garden.seeds,
+                n=2,
+            )
+        ]
+    )
+    for mapping in [
+        garden.seed_to_soil,
+        garden.soil_to_fertilizer,
+        garden.fertilizer_to_water,
+        garden.water_to_light,
+        garden.light_to_temperature,
+        garden.temperature_to_humidity,
+        garden.humidity_to_location,
+    ]:
+        for source_start, dest_start, length in sorted(mapping.ranges):
+            seed_intervals.slice(source_start)
+            seed_intervals.slice(source_start + length)
+        new_seed_intervals = IntervalTree(
+            [
+                Interval(
+                    mapping[interval.begin],
+                    # we need to handle the off by one because
+                    # mapping[interval.end] is somewhere else
+                    # entirely
+                    mapping[interval.end - 1] + 1,
+                )
+                for interval in seed_intervals
+            ]
+        )
+        new_seed_intervals.merge_overlaps()
+        seed_intervals = new_seed_intervals
+    return sorted(seed_intervals)[0].begin
 
 
 def main():
     part_1_test = part1(TEST_INPUT)
     assert part_1_test == 35, part_1_test
     print(part1(REAL_INPUT))
-    part_2_test = part1(TEST_INPUT, True)
+    part_2_test = part2(TEST_INPUT)
     assert part_2_test == 46, part_2_test
-    print(part1(REAL_INPUT, True))
+    print(part2(REAL_INPUT))
 
 
 if __name__ == "__main__":
