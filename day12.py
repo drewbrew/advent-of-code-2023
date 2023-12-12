d50358609485e236ba0808022a4c1721809c9af8
Drew Winstel <drew@hsv.beer>
2023-12-12 08:09:09 -0600

    Use recursion to make day 12 solve in a reasonable amount of time

--- day12.py
+++ day12.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from functools import cache
 
 TEST_INPUT = """???.### 1,1,3
 .??..??...?##. 1,1,3
@@ -66,6 +67,38 @@ def line_meets_survey(survey: list[bool], combinations: tuple[int]) -> bool:
     return found_results == list(combinations)
 
 
+@cache
+def fit_count(survey: str, counts: tuple[int]) -> int:
+    # print(f"{survey=}, {counts=}")
+    if not counts:
+        return int("#" not in survey)
+
+    # what's our biggest group?
+    biggest = max(counts)
+    biggest_index = counts.index(biggest)
+    left_counts, right_counts = counts[:biggest_index], counts[biggest_index + 1 :]
+
+    start_buffer = sum(left_counts) + len(left_counts)
+    end_buffer = sum(right_counts) + len(right_counts)
+
+    total = 0
+    for index in range(start_buffer, len(survey) - end_buffer - biggest + 1):
+        # split at the biggest and recursively check
+        block_pattern = survey[index : index + biggest]
+        left_border = "" if not index else survey[index - 1]
+        right_border = "" if index + biggest == len(survey) else survey[index + biggest]
+        # print(index, block_pattern, left_border, right_border)
+        if "." not in block_pattern and left_border != "#" and right_border != "#":
+            # if we have a valid large block that can be swapped around, find our boundary
+            # counts and multipy them together
+            left_total = fit_count(survey[: index - len(left_border)], left_counts)
+            right_total = fit_count(
+                survey[index + biggest + len(right_border) :], right_counts
+            )
+            total += left_total * right_total
+    return total
+
+
 def valid_combos(survey: str, combinations: tuple[int]) -> int:
     """How many valid combinations fit the survey pattern?"""
     attempts = [""]
@@ -81,7 +114,7 @@ def valid_combos(survey: str, combinations: tuple[int]) -> int:
                 for attempt in attempts
                 if valid_in_progress(attempt, combinations)
             ]
-    print(survey, combinations, len(attempts))
+    # print(survey, combinations, len(attempts))
     # print(combinations, survey, "\n".join(str(i) for i in attempts))
     return sum(
         line_meets_survey(attempt, combinations=combinations) for attempt in attempts
@@ -102,7 +135,7 @@ def part2(puzzle: str) -> int:
     lines = parse_input(puzzle=puzzle, part2=True)
     score = 0
     for survey, combos in lines:
-        s = valid_combos(survey, combos)
+        s = fit_count(survey, combos)
         # print(survey, combos, s)
         score += s
     return score
@@ -111,10 +144,9 @@ def part2(puzzle: str) -> int:
 def main():
     part_1_result = part1(TEST_INPUT)
     assert part_1_result == 21, part_1_result
-    print("p1", part1(REAL_INPUT))
+    print(part1(REAL_INPUT))
     part_2_result = part2(TEST_INPUT)
     assert part_2_result == 525152, part_2_result
-    print("here goes")
     print(part2(REAL_INPUT))
 
 
