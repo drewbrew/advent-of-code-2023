9ae4c8f26d7e4fb5f9ffa35562c5f5f8542ec2a0
Drew Winstel <drew@hsv.beer>
2023-12-13 06:52:56 -0600

    run black

--- day13.py
+++ day13.py
@@ -17,24 +17,24 @@ TEST_INPUT = """#.##..##.
 ..##..###
 #....#..#"""
 
-REAL_INPUT = Path('day13.txt').read_text()
+REAL_INPUT = Path("day13.txt").read_text()
 
 
 def find_horizontal_reflection(puzzle: list[str]) -> int:
-    print(f'finding reflection for {puzzle}')
+    print(f"finding reflection for {puzzle}")
     for y, row in enumerate(puzzle):
         try:
             if puzzle[y + 1] == row:
-                print('yay', y)
+                print("yay", y)
                 # we have a reflection!
                 return y + 1
-                
+
         except IndexError:
-            raise ValueError('end of the line?')
+            raise ValueError("end of the line?")
+
 
 def find_reflection(horizontal_puzzle: str, vertical_puzzle: str) -> tuple[int, int]:
-    """given the puzzle, find the reflection offsets
-    """
+    """given the puzzle, find the reflection offsets"""
     lines = horizontal_puzzle.splitlines()
     # start with the easy option: reflection along the x axis
     vertical_reflection_index = find_horizontal_reflection(lines)
@@ -45,13 +45,14 @@ def find_reflection(horizontal_puzzle: str, vertical_puzzle: str) -> tuple[int, 
         column = []
         for y in range(len(vertical_puzzle_lines)):
             column.append(vertical_puzzle_lines[y][x])
-        vertical_lines.append(''.join(column))
-    print(vertical_puzzle,'\n--\n', vertical_lines)
+        vertical_lines.append("".join(column))
+    print(vertical_puzzle, "\n--\n", vertical_lines)
     horizontal_reflection_index = find_horizontal_reflection(vertical_lines)
     return horizontal_reflection_index, vertical_reflection_index
 
+
 def part1(puzzle: str) -> int:
-    puzzles = puzzle.split('\n\n')
+    puzzles = puzzle.split("\n\n")
     horizontal_score = 0
     vertical_score = 0
     for vertical, horizontal in batched(puzzles, n=2):
@@ -66,5 +67,6 @@ def main():
     assert part_1_score == 405, part_1_score
     print(part1(REAL_INPUT))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
