6edc86f921013461f7fe1808709a773f496f3507
Drew Winstel <drew@hsv.beer>
2023-12-14 09:50:11 -0600

    more readable version of day 14 without the one-liner rotation

--- day14.py
+++ day14.py
@@ -1,5 +1,5 @@
 """day 14: rolling rocks"""
-from functools import cache
+from copy import deepcopy
 from pathlib import Path
 
 # NOTE: Chrome tried to ruin my input by "translating" from Portuguese
@@ -35,13 +35,12 @@ def parse_input(puzzle: str) -> list[list[bool | None]]:
     return grid
 
 
-@cache
-def move_north(puzzle: str) -> str:
+def move_north(grid: list[list[bool | None]]):
     """
     Do a single iteration of moving everything northward that can move north
     """
     # old_grid = grid
-    grid = parse_input(puzzle)
+    grid = deepcopy(grid)
     for y, row in enumerate(grid[1:], start=1):
         for x, rolling in enumerate(row):
             if rolling and grid[y - 1][x] is None:
@@ -62,7 +61,65 @@ def move_north(puzzle: str) -> str:
     # print("---")
     # print_grid(grid)
     # print("====")
-    return puzzle_to_string(grid)
+    return grid
+
+
+def move_south(grid: list[list[bool | None]]) -> list[list[bool | None]]:
+    """
+    Do a single iteration of moving everything southward that can move south
+    """
+    # it's easiest to flip the grid vertically and flip back
+    return list(reversed(move_north(list(reversed(grid)))))
+
+
+def move_east(grid: list[list[bool | None]]) -> list[list[bool | None]]:
+    grid = deepcopy(grid)
+    for y, row in enumerate(grid):
+        row_interim = list(reversed(row))
+        # print(
+        #     puzzle_to_string([row]),
+        #     "became",
+        #     puzzle_to_string([row_interim]),
+        # )
+        # this one is obnoxious: we have to work backwards
+        for x, rolling in enumerate(row_interim[1:], start=1):
+            if rolling and row_interim[x - 1] is None:
+                delta = 1
+                try:
+                    while row_interim[x - delta] is None:
+                        delta += 1
+                        if x - delta < 0:
+                            break
+                except IndexError:
+                    pass
+                delta -= 1
+                row_interim[x - delta] = rolling
+                row_interim[x] = None
+                # print("after swap at ", x, puzzle_to_string([row_interim]))
+        # now save the fixed row
+
+        grid[y] = list(reversed(row_interim))
+        # print("and finally", puzzle_to_string([grid[y]]))
+    return grid
+
+
+def move_west(grid: list[list[bool | None]]) -> list[list[bool | None]]:
+    grid = deepcopy(grid)
+    for row in grid:
+        for x, rolling in enumerate(row[1:], start=1):
+            if rolling and row[x - 1] is None:
+                delta = 1
+                try:
+                    while row[x - delta] is None:
+                        delta += 1
+                        if x - delta < 0:
+                            break
+                except IndexError:
+                    pass
+                delta -= 1
+                row[x - delta] = rolling
+                row[x] = None
+    return grid
 
 
 def puzzle_to_string(grid: list[list[bool | None]]) -> str:
@@ -94,30 +151,38 @@ def print_grid(grid: list[list[bool | None]]):
 
 
 def part1(puzzle: str) -> int:
-    grid = move_north(puzzle)
+    grid = parse_input(puzzle)
+
+    new_grid = move_north(grid)
     score = 0
-    for weight, row in enumerate(reversed(parse_input(grid)), start=1):
+    for weight, row in enumerate(reversed(new_grid), start=1):
         score += weight * sum(rolling is True for rolling in row)
     return score
 
 
-def rotate_90(puzzle: str) -> str:
-    return "\n".join(map("".join, zip(*(puzzle.split())[::-1])))
-
-
 def part2(puzzle: str) -> int:
+    grid = parse_input(puzzle)
     seen: dict[str, int] = {}
     turns = 0
-    original_puzzle = puzzle
     repeated = False
     while turns < 1_000_000_000:
         turns += 1
-        for _ in range(4):
-            puzzle = move_north(puzzle)
-            puzzle = rotate_90(puzzle)
-        if original_puzzle == TEST_INPUT:
+        grid = move_north(grid)
+        # print("done with N")
+        # print_grid(grid)
+        grid = move_west(grid)
+        # print("done with W")
+        # print_grid(grid)
+        grid = move_south(grid)
+        # print("done with S")
+        # print_grid(grid)
+        grid = move_east(grid)
+        # print("done with E")
+        # print_grid(grid)
+
+        if puzzle == TEST_INPUT:
             if turns == 1:
-                stringified = puzzle
+                stringified = puzzle_to_string(grid)
                 assert (
                     stringified
                     == """.....#....
@@ -133,7 +198,7 @@ def part2(puzzle: str) -> int:
                 ), stringified
                 print("turn 1 ok")
             if turns == 2:
-                stringified = puzzle
+                stringified = puzzle_to_string(grid)
                 assert (
                     stringified
                     == """.....#....
@@ -149,7 +214,7 @@ def part2(puzzle: str) -> int:
                 ), stringified
                 print("turn 2 ok")
             if turns == 3:
-                stringified = puzzle
+                stringified = puzzle_to_string(grid)
                 assert (
                     stringified
                     == """.....#....
@@ -164,11 +229,12 @@ def part2(puzzle: str) -> int:
 #.OOO#...O"""
                 ), stringified
                 print("turn 3 ok")
+        new_grid = puzzle_to_string(grid)
         if not repeated:
             try:
-                old_turn = seen[puzzle]
+                old_turn = seen[new_grid]
             except KeyError:
-                seen[puzzle] = turns
+                seen[new_grid] = turns
             else:
                 print("repeat after turn", turns, "from", old_turn)
                 repeated = True
@@ -178,7 +244,7 @@ def part2(puzzle: str) -> int:
                 turns -= delta
 
     score = 0
-    for weight, row in enumerate(reversed(parse_input(puzzle)), start=1):
+    for weight, row in enumerate(reversed(grid), start=1):
         score += weight * sum(rolling is True for rolling in row)
     return score
 
